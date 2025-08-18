b838c0680603c563f062956f317bbe8cb145b20a
besrex1989 <besar.rexhepaj@hotmail.com>
2025-08-18 10:43:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -173,5 +173,6 @@ def dashboard():
                            monthly=monthly)
 
 # -----------------------------------------------------------------------------
-if __name__=='__main__':
-    app.run(host="0.0.0.0",port=5050,debug=True)
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host="0.0.0.0", port=port)
